dec00728b60ca0cff8f08c3bfdc7f462753c596f
damienwmartin <damienwilchardmartin@gmail.com>
2020-11-01 11:57:24 -0500

    readme and gamewrapper

---
+++ Modified_Rebel/Readme.md
@@ -0,0 +1,10 @@
+# Python Rebel Implementation
+
+Overview
+- Rebel_Alg -> Runs through the Rebel whold rebel algorith
+- game_tree -> Builds subgame trees as a directed graph, (game wrapper is currently in here as well i want to move it)
+- PBS -> class for PBS that serves as search tree nodes
+
+Still need to do
+- plug in a cfr implementation
+- Game wrapper
--- Modified_Rebel/game_tree.py
+++ Modified_Rebel/game_tree.py
@@ -1,14 +1,29 @@
 import networkx as nx
+'''
+Implentation for the game trees and wrappers for different game libraries
+ReBeL only interacts with the subgame tree so if we place the wrapper here the rest should be application independant
+'''
 
 
 class game_wrapper():
-	
+	'''
+	Base class for game wrappers
+	'''
 	def get_legal_moves(game_state):
+		#Needs to take a game state and return legal moves for the next player 
 		pass
-
 	def get_rewards(game_state):
+		#Needs to take a terminal game state and return rewards for each player
 		pass
-
+	def pbs2gamestate(PBS):
+		#Converts PBS into a readable game state
+		pass
+	def gamestate2public_state(game_state):
+		#Converts gamestate to PBS public state
+		pass
+	def take_action(game_state, action):
+		#returns next public state
+		pass
 
 
 
@@ -16,7 +31,8 @@ class game_tree():
 	'''
 	Game tree implemented as a networkx directed graph
 	Node names are (depth from root, child number)
-	Node attributes are dict -> depth, state
+	Node attributes are dict -> depth, state (PBS)
+	Edge atributes are actions 
 	'''
 	def __init__(self, game_wrapper):
 		self.game = game_wrapper
@@ -32,12 +48,12 @@ class game_tree():
 		for i in range(depth+1):
 			#Check every node in the queue
 			for node in node_queue:
-
 				#Mark as terminal if there are no legal moves
 				legal_moves = self.game.get_legal_moves(game_state)
 
 				if legal_moves == None:
 					self.tree.nodes[node]['terminal'] = True
+					self.tree.nodes[node]['value'] = self.game.get_rewards(game)
 
 				#otherwise expand the tree until we hit the desired depth
 				else:
@@ -49,8 +65,9 @@ class game_tree():
 							next_node_queue.append((i+1, j))
 			node_queue = next_node_queue
 	
-	def set_leaf_values(policy, value_net):
-		raise (NotImplementedError)
+	def set_leaf_values(self, policy, value_net):
+		node_list = [self.tree.nodes[(0,0)]]
+
 
 	def sample_leaf(self, policy)
 		raise(NotImplementedError)
