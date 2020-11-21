6be0cc06387409b9449c5a13620a7a869f216850
damienwmartin <damienwilchardmartin@gmail.com>
2020-11-21 18:57:26 -0500

    Recursive subgame tree, initialize policy, simplified PBS

--- Rebel/game_tree.py
+++ Rebel/game_tree.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import numpy as np
 '''
 Implentation for the game trees and wrappers for different game libraries
 ReBeL only interacts with the subgame tree so if we place the wrapper here the rest should be application independant
@@ -8,54 +9,50 @@ Note:
 - Leaf nodes are terminal in the subgame but not in the full game
 '''
 
-class game_tree():
+class recursive_game_tree():
 	'''
-	Game tree implemented as a networkx directed graph
-	Node names are (depth from root, child number)
-	Node attributes are dict -> depth, state (PBS)
-	Edge atributes are actions 
+	game tree implementation as NetworkX Directed Graph
+
+	Node Ids are tuple with all actions taken from root e.g. ('root', 'raise', 'call', 'fold')
+	
+	Node Attributes
+	-Depth
+	-PBS
+	-Policy
+
 	'''
-	def __init__(self, game_wrapper):
-		self.game = game_wrapper
-		self.tree = nx.DiGraph()
-	
-	#build subgame tree with depth t
-	def construct_subgame(self, PBS, depth):
-		self.tree.add_node((0, 0), depth=0, PBS=PBS)
-		node_queue = [(0,0)]
+	def __init__ (self, PBS): 
+		self.tree.add_node(('root',), depth=0, PBS=PBS)
 
-		#for every range up to desired depth
-		for i in range(depth+1):
-			next_node_queue = []
-			#Check every node in the queue
-			for xy in node_queue:
-				node = self.tree.node(xy)
-				#Mark as terminal if there are no legal moves
-				game_state = self.game.pbs2gamestate(node['PBS'])
-				legal_moves = self.game.get_legal_moves(game_state)
 
-				if legal_moves == None:
-					node['terminal'] = True
-					node['subgame_terminal'] = False
-					node['value'] = self.game.get_rewards(game)
+	def build_depth_limited_subgame(self, depth_limit = 5, node_id = ('root',)):
+		'''
+		Recursively expand all nodes in the tree until you hit the depth limit
+		Mark nodes which are subgame terminal
+		'''
+		node = self.tree.nodes[node_id]
+		if node['depth'] < depth_limit:
+			node['subgame_terminal'] = False
+			self.expand_node(node_id)
+			children = list(self.tree.successors(node_id))
+			for child in children:
+				self.build_depth_limited_subgame(depth_limit, child)
+		if node['depth'] == depth_limit:
+			node['subgame_terminal'] = True
 
-				#otherwise expand the tree until we hit the desired depth
-				else:
-					node['terminal'] = False
-					if i != depth:
-						node['subgame_terminal']=False
-						for j, action in enumerate(legal_moves):
-							self.tree.add_node((i+1, j),  {'depth':i+1, state = node['PBS'].transition(action)})
-							self.tree.add_edge(node, (i+1, j), action = action)
-							next_node_queue.append((i+1, j))
-					else:
-						node['subgame_terminal'] = True
-			node_queue = next_node_queue.copy()
-	
-	def set_leaf_values(self, policy, value_net, node=None):
-		#start at root if no node is given
-		if node == None:
-			node = self.tree.nodes[(0,0)]
+	def initialize_random_policy(self):
+		''''
+		Run through the tree and set all node policies to be uniform over available actions
+		Policy is (actions x infostates)
+		'''
+		for node_id, attributes in G.nodes.data():
+			actions = self.game.get_legal_moves(attributes['PBS'])
+			policy = {action:np.ones(self.game.num_infostates)/len(actions)}
+
+	def set_leaf_values(self, policy, value_net, node_id=('root',)):
+		#TODO: This is incomplete
+		node = self.tree.nodes[node_id] 
+		
 
 		#Estimate values for nodes terminal in the subgame
 		if node['subgame_terminal']:
@@ -68,26 +65,91 @@ class game_tree():
 				self.tree.add_node((i+1, j),  {'depth':i+1, state = node['PBS'].transition(action)})
 				self.tree.add_edge(node, (i+1, j), action = action)
 				self.set_leaf_values(policy, value_net, self.tree.nodes[(i,j)]) #TODO the policy being passed here is wrong
-					
 
 
-	def sample_leaf(self, policy)
-		#Probabilistically run through the subgame tree until you hit a leaf node and return is to start new subgame
-		raise(NotImplementedError)
+	def expand_node(self, node_id):
+		# Spawn a child node for every available actions
+		legal_actions = self.game.get_legal_moves(pbs)
+		
+		for action in legal_actions:
+			self.add_child(node_id, action)
+
+
+	def add_child(self, node_id, action):
+		'''
+		get next PBS after taking action
+		add PBS node to tree
+		add edge from parent to chile
+		'''
+		node = self.tree.nodes[node_id]
+
+		new_pbs = self.transition(node['PBS'], action)
+		
+		new_node_id = (*node_id, action)
+
+		new_depth =  node['depth'] + 1
+
+		self.tree.add_node(new_node_id, depth = new_depth, PBS = new_pbs)
+		self.tree.add_edges(node_id, new_node_id, action=action)
+
+
+	def transition(self, pbs, action):
+		'''
+		given a PBS and an actions spawn the next PBS
+		'''
+		#reach out to the game wrapper for the next public state
+		next_public_state = self.game.take_action(action, pbs) 
+		
+		#Get the next distribution of infostates
+		next_infostate_probs = pbs.update_infostate_probs(policy, action, player_number)
+
+		#return a new updated pbs after taking action
+		return(PBS(next_public_state, next_infostate_probs))
+
+
+
+
+class PBS():
+	'''
+	Containts all public knowledge of knowledge and probability distribution for each players infostates
+	For liars dice public is players turn, last bid, and probability distribution of each players hands
+	'''
+	def __init__(self, public_state, infostate_probs):
+		
+		#wrapper for the specific game we are playing
+		self.game = game_wrapper
+		#representation of public state for the game
+		self.public = public_state
+		#list of probability matrices for each players infostate, (# players, infostate size)
+		self.infostate_probs = infostate_probs 		
+		
+	def update_infostate_probs(self, policy, action, player_number):
+		'''
+		policy is for the state - size (# infostates, actions)
+		beyes update infostate posterior(state) ~ prior(state)*p(a|state)
+		action is by the other player
+		player_number is whose beliefs you're updating
+
+		'''
+		new_infostate_probs = self.infostate_probs.copy()
+		new_infostate_probs[player_number] = self.infostate_probs[player_number] * policy[action]/sum(self.infostate_probs[player_number] * policy[action])
+		return(new_infostate_probs)
+
+
+	def vector(self):
+		'''
+		Numpy array representation -> input into value/policy net
+		'''
+		return(np.concat([self.public_state, *self.infostate_probs]))
+
+#This is being moved into the game tree
+def set_leaf_values(PBS, Policies, value_net):
+	#If node ends in subgame but not the full game predict value with v_net
 	
-	def compute_ev(policy):
-		raise(NotImplementedError)
-
-
-class recursive_game_tree():
-	#TODO: Game tree can be implemented way cleaner
-	def __init__ (self, PBS): 
-		self.tree.add_node((0, 0), depth=0, PBS=PBS, )
-
-	def expand_node(node = None):
-		
-
-	def update_node(node = None):
-		
-
-
+	if is_leaf(PBS):
+		vs = value_net(PBS.vector)
+		game_tree.add_node(pbs = PBS, value = vs)
+	
+	else:
+		for action in possible_actions:
+			set_leaf_values(PBS.transition(action, policy, 1-player_number))
