bc1d570855a1d4774fea28e052e48e39cbd0b7f6
damienwmartin <damienwilchardmartin@gmail.com>
2020-11-21 22:58:33 -0500

    added sample leaf, compute ev, set leaf value

--- Rebel/game_tree.py
+++ Rebel/game_tree.py
@@ -49,22 +49,68 @@ class recursive_game_tree():
 			actions = self.game.get_legal_moves(attributes['PBS'])
 			policy = {action:np.ones(self.game.num_infostates)/len(actions)}
 
-	def set_leaf_values(self, policy, value_net, node_id=('root',)):
+	def set_leaf_values(self, value_net, node_id=('root',)):
 		#TODO: This is incomplete
 		node = self.tree.nodes[node_id] 
 		
 
 		#Estimate values for nodes terminal in the subgame
-		if node['subgame_terminal']:
+		if node['subgame_terminal'] and  not node['terminal']:
 			node['value'] = value_net(node['PBS'].vector())
 
 		#Otherwise recursively rebuild the tree
 		
-		elif (i := node['depth']) < self.max_depth:
-			for j, action in enumerate(legal_moves):
-				self.tree.add_node((i+1, j),  {'depth':i+1, state = node['PBS'].transition(action)})
-				self.tree.add_edge(node, (i+1, j), action = action)
-				self.set_leaf_values(policy, value_net, self.tree.nodes[(i,j)]) #TODO the policy being passed here is wrong
+		else:
+			for action in self.game.get_legal_moves(node['PBS']):
+				self.add_child(node_id, action)
+				self.set_leaf_values(value_net, (*node_id,action))
+
+
+	def compute_ev(self, node_id = ('root',)):
+		node = self.tree.nodes[node_id]
+		
+		if node['subgame_terminal'] or node['terminal']:
+			return(node['value'])
+
+		else:
+			node['value']=0
+			for action in self.game.get_legal_moves(['PBS']):
+				#sum(p(a)*v(T(s,a)))
+				node['value'] += node['policy']['action'].sum()*self.compute_ev(self.tree.nodes[(*node_id,action)]) 
+
+
+	def sample_leaf(self):
+		node_id = ('root',)
+		node = self.tree.nodes[('root',)]
+		random_player = np.random.choice([0,1])
+		#TODO: figure out exactly how histories are represented
+		h = self.game.sample_history(node)
+		
+		while not node['terminal'] and not node['subgame_terminal']:
+			if random_player and (np.random.random()<.25):
+				node = self.sample_child(node, h, random=True)
+			else:
+				node = self.sample_child(node,h)
+			random_player =  not random_player
+
+		return(node)
+
+
+	def sample_child(self, node, infostate=None, random=False):
+		#TODO: Make the policy a matrix instead of dict
+		if random:
+			probs = None
+
+		elif infostate!=None:
+			probs = node[:,'policy']
+		else:
+			probs = node['policy'].sum(axis=1) 
+
+		children = list(self.tree.successors(node_id))
+		child_id = np.random.choice(children, probs)
+		return(self.tree.nodes[child_id])
+
+
 
 
 	def expand_node(self, node_id):
@@ -79,29 +125,39 @@ class recursive_game_tree():
 		'''
 		get next PBS after taking action
 		add PBS node to tree
-		add edge from parent to chile
+		add edge from parent to child
+		if child is terminal get the value from the game
 		'''
 		node = self.tree.nodes[node_id]
 
-		new_pbs = self.transition(node['PBS'], action)
+		new_pbs = self.transition(node['PBS'], action, node['policy'])
 		
 		new_node_id = (*node_id, action)
 
 		new_depth =  node['depth'] + 1
 
-		self.tree.add_node(new_node_id, depth = new_depth, PBS = new_pbs)
+		#If terminal get the payouts from the game and weight by probability of history
+		if terminal := self.game.is_terminal(new_pbs):
+			value = np.multiply(self.game.infostate_values(new_pbs), new_pbs.infostate_matrix).sum().sum()
+
+		self.tree.add_node(new_node_id, depth = new_depth, PBS = new_pbs, terminal=terminal)
 		self.tree.add_edges(node_id, new_node_id, action=action)
 
 
-	def transition(self, pbs, action):
+	def transition(self, pbs, action, policy = None):
 		'''
 		given a PBS and an actions spawn the next PBS
 		'''
 		#reach out to the game wrapper for the next public state
 		next_public_state = self.game.take_action(action, pbs) 
 		
-		#Get the next distribution of infostates
-		next_infostate_probs = pbs.update_infostate_probs(policy, action, player_number)
+		#Get the next distribution of infostates if policy exists
+		if policy != None:
+			next_infostate_probs = pbs.update_infostate_probs(policy, action, player_number)
+
+		#If playing with a random policy nothing is learned
+		else:
+			next_infostate_probs = pbs.infostate_probs
 
 		#return a new updated pbs after taking action
 		return(PBS(next_public_state, next_infostate_probs))
@@ -134,6 +190,12 @@ class PBS():
 		new_infostate_probs = self.infostate_probs.copy()
 		new_infostate_probs[player_number] = self.infostate_probs[player_number] * policy[action]/sum(self.infostate_probs[player_number] * policy[action])
 		return(new_infostate_probs)
+
+	def infostate_matrix(self):
+		'''
+		Joint probability of each players possible hands give infostate. 
+		'''
+		return(np.self.infostate_probs[0].T,self.infostate_probs[1])
 
 
 	def vector(self):
